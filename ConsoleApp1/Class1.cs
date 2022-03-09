90ba586a67c3944b70275a94c65196d7305f6975
akankshalanjewar <akankshalanjewar02@gmail.com>
2022-03-09 15:49:34 +0530

    added[akanksha]line

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Class1
+    {
+        public void Length()
+        {
+            double x1, x2, y1, y2;
+            double f1, f2, g1, g2;
+
+            Console.WriteLine("Enter co-ordinate of line1");
+            Console.WriteLine("Enter co-ordinate of (x1,y1)");
+            x1 = double.Parse(Console.ReadLine());
+            y1 = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Enter co-ordinate of line1");
+            Console.WriteLine("Enter co-ordinate of (x2,y2)");
+            x2 = double.Parse(Console.ReadLine());
+            y2 = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Enter co-ordinate of line2");
+            Console.WriteLine("Enter co-ordinate of (f1,g1)");
+            f1 = double.Parse(Console.ReadLine());
+            g1 = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Enter co-ordinate of line2");
+            Console.WriteLine("Enter co-ordinate of (f2,g2)");
+            f2 = double.Parse(Console.ReadLine());
+            g2 = double.Parse(Console.ReadLine());
+
+
+            double Length1 = Math.Sqrt(((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)));
+            double Length2 = Math.Sqrt(((f2 - f1) * (f2 - f1) + (g2 - g1) * (g2 - g1)));
+
+
+            Console.WriteLine("Length of line1 is" + Length1);
+            Console.WriteLine("Length of line2 is" + Length2);
+
+
+        }
+    }
+}
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1 
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Class1 obj = new Class1();
+            obj.Length();
+
+        }
+    }   } 
+
